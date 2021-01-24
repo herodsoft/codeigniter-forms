1cab9c3310bf512834748b18154b658c93618526
hrodriguez <hrodriguez@mpaycenter.com>
2021-01-24 14:27:45 -0500

    RequestHandler for know if the forma is submitted

--- src/FormType.php
+++ src/FormType.php
@@ -4,12 +4,16 @@
 namespace Forms\CI;
 
 
+use CodeIgniter\HTTP\Request;
 use Forms\CI\Types\InputType;
 use Forms\CI\Types\TypeInputInterface;
 
 abstract class FormType implements FormTypeInterface
 {
     protected array $inputs = [];
+    protected bool $isSubmited = false;
+    protected bool $isValidated = false;
+    protected Request $request;
 
 
     public function __construct()
@@ -28,11 +32,69 @@ abstract class FormType implements FormTypeInterface
         return $form . form_close();
     }
 
-    public function add(string $name, TypeInputInterface $type)
+    public function addInput(string $name, TypeInputInterface $type)
     {
         $this->inputs[$name] = $type;
         return $this;
     }
+
+    /**
+     * @return bool
+     */
+    public function isValidated(): bool
+    {
+        if($this->request)
+
+        return $this->isValidated;
+    }
+
+    /**
+     * @param bool $isValidated
+     */
+    protected function setIsValidated(bool $isValidated): void
+    {
+        $this->isValidated = $isValidated;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getRequestHandler(): string
+    {
+        return $this->request;
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setRequestHandler(Request $request): void
+    {
+        $this->request = $request;
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function isSubmited(): bool
+    {
+        if( !empty($this->request) && is_a($this->request, Request::class) && $this->request->getMethod() != 'get')
+        {
+            $this->setIsSubmited(true);
+        }
+        return $this->isSubmited;
+    }
+
+    /**
+     * @param bool $isSubmited
+     */
+    public function setIsSubmited(bool $isSubmited): void
+    {
+
+        $this->isSubmited = $isSubmited;
+    }
+
 
     /**
      * @return array
