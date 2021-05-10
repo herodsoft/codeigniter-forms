e82199b835084e93fa33e711155eb42f16fde229
hrodriguez <hrodriguez@mpaycenter.com>
2021-05-10 01:27:31 -0500

    Add Validation From Request

--- src/FormType.php
+++ src/FormType.php
@@ -4,6 +4,7 @@
 namespace Forms\CI;
 
 
+use CodeIgniter\Config\Services;
 use CodeIgniter\HTTP\Request;
 use Forms\CI\Types\InputType;
 use Forms\CI\Types\TypeInputInterface;
@@ -14,6 +15,14 @@ abstract class FormType implements FormTypeInterface
     protected bool $isSubmited = false;
     protected bool $isValidated = false;
     protected Request $request;
+    protected string $action = ''; //route_name
+    protected array $attributes = ['class'=>'', 'id'=>''];
+    protected array $hiddenInputs = [];
+    protected bool $multiPartForm  = false;
+    protected string $validationRule = '';
+    protected bool $skipValidation = false;
+    protected array $validationRules      = [];
+    protected array $validationErrors = [];
 
 
     public function __construct()
@@ -24,7 +33,21 @@ abstract class FormType implements FormTypeInterface
 
     public function buildView(): string
     {
-        $form = form_open(current_url());
+
+        if(!empty($this->getAction()))
+        {
+            $this->setAction(route_to($this->getAction()));
+        }
+
+        if($this->multiPartForm)
+        {
+            $form = form_open_multipart($this->getAction()??current_url(), $this->getAttributes(), $this->getHiddenInputs());
+
+        }else
+        {
+            $form = form_open($this->getAction()??current_url(), $this->getAttributes(), $this->getHiddenInputs());
+        }
+
         foreach ($this->inputs as $key =>  $value)
         {
             $form .= $this->getInput($key)->buildType()."<br>";
@@ -38,13 +61,61 @@ abstract class FormType implements FormTypeInterface
         return $this;
     }
 
+
+
+
+    protected function runValidation()
+    {
+        $validation =  Services::validation();
+        if(!$this->skipValidation)
+        {
+            if(!empty($this->validationRule) && !empty($this->request))
+            {
+                $rule = $this->validationRule;
+                $validation->setRuleGroup($rule);
+
+                $resultValidation =
+                    $validation
+                    ->withRequest($this->request)
+                    ->run();
+                $this->setIsValidated($resultValidation);
+
+                if(!$resultValidation)
+                {
+                    $this->validationErrors = $validation->getErrors();
+                }
+
+                return $resultValidation;
+
+            }
+
+            if(count($this->validationRules) && !empty($this>$this->request))
+            {
+                $validation->reset();
+
+
+                $resultValidation =  $validation
+                    ->setRules($this->validationRules)
+                    ->withRequest($this->request)
+                    ->run();
+                $this->setIsValidated($resultValidation);
+
+                if(!$resultValidation)
+                {
+                    $this->validationErrors = $validation->getErrors();
+                }
+
+                return $resultValidation;
+            }
+        }
+    }
+
     /**
      * @return bool
      */
     public function isValidated(): bool
     {
-        if($this->request)
-
+        $this->runValidation();
         return $this->isValidated;
     }
 
@@ -122,6 +193,70 @@ abstract class FormType implements FormTypeInterface
     {
         $this->inputs = $inputs;
     }
+
+    /**
+     * @return string
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $action
+     */
+    public function setAction(string $action): void
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @return array|string[]
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @param array|string[] $attributes
+     */
+    public function setAttributes(array $attributes): void
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHiddenInputs(): array
+    {
+        return $this->hiddenInputs;
+    }
+
+    /**
+     * @param array $hiddenInputs
+     */
+    public function setHiddenInputs(array $hiddenInputs): void
+    {
+        $this->hiddenInputs = $hiddenInputs;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValidationErrors(): array
+    {
+        return $this->validationErrors;
+    }
+
+
+
+
+
+
+
+
 
 
 
